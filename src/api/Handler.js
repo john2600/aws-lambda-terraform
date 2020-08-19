5a83c0e2a6b31f5e42c8a44518a91dee9cc27268
john2600 <jsbonilla2600@gmai.com>
2020-08-19 09:56:41 -0500

    part 2

--- src/api/Handler.js
+++ src/api/Handler.js
@@ -1,18 +1,20 @@
 const Result = require('./Result');
+const logger = require('../logger/Logger').logger;
+
 class Handler {
     constructor(bCalculatorService){
-        this.handleCalculation = async(event, context, callback)=>{
-            console.log('Handle Calculatin json='+JSON.stringify(event));
+        this.handleCalculation = async(event, context, callback)=>{           
+            logger.info('Handle Calculatin json result='+JSON.stringify(event));
             try{
                 let weight = event.queryStringParameters.weight;
                 let height = event.queryStringParameters.height;
-
                 if(isNaN(weight) || isNaN(height)){
                     return new Result.BadRequest_400('error trying to proccess values');
                 }
                 let  bmiResult = await this.bCalculatorService.performCalculation(weight, height);
                 return new Result.OK_200(bmiResult);
             }catch(e){
+                logger.error(`error has ocurred during calculation : message=${e}`)
                 return new Result.InternalServerError_500(e);
            }
         };
@@ -20,4 +22,3 @@ class Handler {
     }
 }
 exports.Handler = Handler;
-
